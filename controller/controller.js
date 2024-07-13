308c7d3c6369010bc5eb243002c06118344bcb85
rahulsai121 <pasam.rahul1234@gmail.com>
2024-07-13 21:00:06 +0530

    bcrypt

--- controller/controller.js
+++ controller/controller.js
@@ -1,30 +1,38 @@
-const { where } = require('sequelize')
+const bcrypt = require('bcrypt');
+
 const user=require('../model/user')
 exports.userpost= async (req, res) => {
     //console.log(req.body)
+
+    
+    const hash=await bcrypt.hash(req.body.password,10)
     const newuser= await user.create({
         name:req.body.name,
         email:req.body.email,
-        password:req.body.password
+        password:hash
     })
 
-    //res.send(newpost.dataValues)
+    res.status(201).json({ message: 'User created successfully'});
 }
 
 exports.loginpost=async(req,res)=>{
-    console.log(req.body)
+
     const userdata= await user.findAll({where:{email:req.body.email}})
-    console.log('---------',userdata)
+    
+    console.log('-------------',userdata)
     
     if(userdata.length>0){
-        if(userdata[0].password===req.body.password){
-            res.send({message:'User login successfull'})
+        
+        const match=await bcrypt.compare(req.body.password,userdata[0].password)
+        console.log('match------------',match)
+        if(match){
+            res.status(200).json({message:'User login successfull'})
         }
         else{
-            res.send({message:'User not authorized'})
+            res.status(401).json({message:'User not authorized'})
         }
     }
     else{
-        res.send({message:'User not found'})
+        return res.status(404).json({message:'User not found'})
     }
 }
